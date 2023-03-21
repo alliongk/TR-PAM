6d25386b48c5580aca0e0e7c001b0d6e420be120
All <alliong75@gmail.com>
2023-03-22 01:41:21 +0700

    Upload
    Upload

---
+++ app/src/main/java/com/example/trpam/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.example.trpam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    public static int LIMIT=2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+            startActivity(intent);
+            finish();
+        },LIMIT);
+    }
+}
